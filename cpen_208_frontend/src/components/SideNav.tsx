18231ab6ef98d111f591639b2c7aaf15a7ad2a2a
Samuel Kojo Anafi Antwi <samuelantwi103@gmail.com>
2024-06-15 08:16:06 +0000

    adding highlight to side nav

--- cpen_208_frontend/src/components/SideNav.tsx
+++ cpen_208_frontend/src/components/SideNav.tsx
@@ -5,9 +5,15 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 const side_link = [
-  { name: "Dashboard", href: "/samuel/dashboard" },
-  { name: "Courses", href: "/samuel/courses" },
-  { name: "Finances", href: "/samuel/finances" },
+  [
+    { name: "Dashboard", href: "/samuel/dashboard" },
+    { name: "Courses", href: "/samuel/courses" },
+    { name: "Finances", href: "/samuel/finances" },
+  ],
+  [
+    { name: "Profile", href: "/samuel" },
+    { name: "Settings", href: "/samuel/settings" },
+  ],
 ];
 
 export default function SideNav() {
@@ -15,20 +21,34 @@ export default function SideNav() {
   return (
     <div className="flex py-10  md:overflow-y-auto  flex-col justify-between space-y-5 bg-[hsl(197,27%,67%)] h-[91%] sticky top-0 rounded-tr-3xl">
       <div className={clsx("flex flex-col")}>
-        {side_link.map((link) => {
+        {side_link[0].map((link) => {
           return (
-            <div key={link.name}className={clsx("md:px-12 py-4",{ "text-[#0A7AAA] bg-[#E5F4FA] ": pathname === link.href })}>
-              <Link href={link.href}>{link.name}</Link>
-            </div>
+            <Link
+              href={link.href}
+              key={link.name}
+              className={clsx("md:px-12 py-4", {
+                "text-[#0A7AAA] bg-[#E5F4FA] ": pathname === link.href,
+              })}
+            >
+              <div>{link.name}</div>
+            </Link>
           );
         })}
       </div>
       <div className="flex flex-col pb-5 ">
-        <div className="md:px-12 py-4">Profile</div>
-        <div className="md:px-12 ">
-          {/* <Image src="src" alt="alt" width={50} height={50} /> */}
-          <div>Settings</div>
-        </div>
+        {side_link[1].map((link) => {
+          return (
+            <Link
+              href={link.href}
+              key={link.name}
+              className={clsx("md:px-12 py-4", {
+                "text-[#0A7AAA] bg-[#E5F4FA] ": pathname === link.href,
+              })}
+            >
+              <div>{link.name}</div>
+            </Link>
+          );
+        })}
       </div>
     </div>
   );
